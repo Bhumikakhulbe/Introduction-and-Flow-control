d6957031cca3cbff377da1dfaedfa096914d97e6
Bhumikakhulbe <bhumikakhulbe3@gmail.com>
2024-05-07 19:31:24 +0530

    module1.2.py

--- module1.2.py
+++ module1.2.py
@@ -1,9 +1,12 @@
-import random
-print("This is a 'NUMBER GUESSING GAME'.\nA secret number will be generated between 0 to 100.\nYou have 6 attempts to guess that number.\nALL THE BEST!")
-x=random.randint(0,100)  #This function is used to generate a random number between the given range
-for i in range(6):
+import random #this module is imported to access the randint() function
+print("This is a 'NUMBER GUESSING GAME'.\nA secret number will be generated between the range specified by you.\nYou have 6 attempts to guess that number.\nALL THE BEST!")
+print("\nPlease select the range of the numbers between which you want to guess to number ")
+lower=int(input("Enter the starting number: "))
+upper=int(input("Enter the ending number: "))
+x=random.randint(lower,upper)  #generates a random number between the given range
+for i in range(6): #the loop will run 6 times 
     y=int(input("\nEnter your guess: "))
-    if(y>100 or y<0):
+    if(y>upper or y<lower): #checks if the entered number is either present between the specified range or not
         print("The entered number is out of the given range")
     elif (x<y):
         print("Try again!\nThe secret number is smaller than this number.")
@@ -11,5 +14,5 @@ for i in range(6):
         print("Try again!\nThe secret number is larger than this number.")
     else:
         print("congratulations!\nYou guessed the correct number.")
-        break
+        break  #the loop will break    
 print("\nThe game is over.\nThankyou for playing this game!")
